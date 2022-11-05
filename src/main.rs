9904fc31dfd36632f41ea18276ed161b2f9d6bf2
Trenton Ornelas <trentonornelas@gmail.com>
2022-11-05 17:35:22 -0500

    Add tuple struct example

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,7 @@
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
 
 fn main() {
-    let mut user1 = User {
-        email: String::from("someone@example.com"),
-        username: String::from("someusernam123"),
-        active: true,
-        sign_in_count: 1,
-    };
-
-    user1.email = String::from("adifferentemail@example.com");
-
-    // we moved user1 data to user2, no longer able to call user1
-    let user2 = User {
-        email: String::from("differentemail@example.com"),
-        ..user1
-    };
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        username,
-        email,
-        active: true,
-        sign_in_count: 1,
-    }
+    let black = Color(0, 0, 0);
+    let origin = Point(0,0,0);
 }
